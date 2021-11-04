39b7c5cbdd4b19ca63aebef519ed1fa3586e2645
Evan Battaglia <ebattaglia@instructure.com>
2021-11-03 22:44:41 -0600

    echo_only again

--- lib/tabry/cli/util.rb
+++ lib/tabry/cli/util.rb
@@ -5,21 +5,23 @@ module Tabry
   module CLI
     module Util
       module_function
-      def make_cmdline(cmdline, *args, echo: false)
+      def make_cmdline(cmdline, *args, echo: false, echo_only: false)
         args = Array(args).flatten
         cmdline = cmdline % args.map{|a| Shellwords.escape(a)}
-        STDERR.puts cmdline if echo
+        STDERR.puts cmdline if echo || echo_only
+        return nil if echo_only
         cmdline
       end
 
-      def system(*cmdline, echo: false, echo_only: false, **opts)
-        cmdline = make_cmdline(*cmdline, **opts, echo: echo || echo_only)
-        Kernel.system cmdline unless echo_only
+      def system(*cmdline, **opts)
+        cmdline = make_cmdline(*cmdline, **opts)
+        Kernel.system cmdline unless cmdline
       end
 
       # TODO would be nice to get separate STDERR and STDOUT
       def backtick_or_die(*cmdline, **opts)
         cmdline = make_cmdline(*cmdline, **opts)
+        return unless cmdline
         res = `#{cmdline}`
         if !$?.success?
           STDERR.puts "COMMAND FAILED with exit code #{$?.exitstatus}: #{cmdline}"
