87f6c68760842b5fbc96ee7af8bdb1082d70debc
Evan Battaglia <ebattaglia@instructure.com>
2022-08-12 05:14:01 -0600

    more specs

--- spec/tabry/cli/builder_spec.rb
+++ spec/tabry/cli/builder_spec.rb
@@ -5,31 +5,96 @@ require_relative '../../../lib/tabry/cli/base.rb'
 
 class Tabry::CLI::Builder
   class TestCli < Tabry::CLI::Base
+    def << self
+      attr_accessor :actions_run, :cli_object
+    end
+
     before_action :my_before_action
     after_action :my_after_action
 
     def main
+      self.actions_run << :main
     end
 
     def build
+      self.actions_run << :build
+    end
+
+    def my_action
+      self.actions_run << :my_action
+    end
+
+    def sub__sub_sub
+      self.actions_run << :sub__sub_sub
     end
 
     def my_before_action
+      self.actions_run << :before_action
+      self.cli_object = self
     end
 
     def my_after_action
+      self.actions_run << :after_action
     end
   end
 end
 
 describe Tabry::CLI::Builder do
-  it 'runs the Machine and loads the Result'
-  it 'quits and shows an error if there is incorrect usage'
-  it "quits and shows an error if there is the command is valid but the CLI class doesn't implement it"
-  it 'runs before actions'
-  it 'runs after actions'
-  it 'handles subcommand routing to other classes'
-  it 'can handle multiple levels of subcommand routing'
-  it 'provides an ArgProxy in TestCLI#args'
-  it 'provides access to flags in TestCLI#flags'
+  before do
+    runner = instance_double(Tabry::Runner)
+    allow(Tabry::Runner).to receive(:new).with(config_name: 'theconfigname').and_return runner
+    allow(runner).to receive(:parse).with(%w[the raw args]).and_return result
+  end
+
+  let(:result) do
+    instance_double(
+      Tabry::Result,
+      state: Tabry::State.new(state.marge(args: named_args.values)),
+      help?: help?,
+      invalid_usage_reason: invalid_usage_reason,
+      named_args: named_args
+    )
+  end
+  let(:help?) { nil }
+  let(:invalid_usage_reason) { nil }
+  let(:named_args) { {} }
+
+  subject do
+    described_class.new('theconfigname', Tabry::CLI::Builder::TestCli).run(%w[the raw args])
+  end
+
+  let(:cli_class) { Tabry::CLI::Builder::TestCli }
+  let(:actions_run) { cli_class.actions_run }
+  let(:cli_object) { cli_class.cli_object}
+
+  before do
+    cli_class.actions_run = []
+    cli_class.cli_object = nil
+  end
+
+  describe 'successful runs' do
+    before { subject }
+
+    describe 'runs the method name, and before and after actions' do
+      let(:state) { { subcommand_stack: %w[my_action] } }
+      it { expect(actions_run).to eq %i[before_action build after_action] }
+    end
+
+    describe 'changing - into _ in subcommand names' do
+      let(:state) { { subcommand_stack: %w[my_action] } }
+      it { expect(actions_run).to eq %i[before_action my_action after_action] }
+    end
+
+    it 'calls main as the main subcommand'
+    it 'can handle multiple levels of subcommand routing'
+    it 'handles subcommand routing to other classes'
+    it 'provides an ArgProxy in TestCLI#args'
+    it 'provides access to flags in TestCLI#flags'
+  end
+
+  describe 'unsuccessful or help runs' do
+    it 'quits and shows an error if there is incorrect usage'
+    it 'quits and shows usage if help is passed in'
+    it "quits and shows an error if there is the command is valid but the CLI class doesn't implement it"
+  end
 end
---
+++ spec/tabry/runner_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../../lib/tabry/runner.rb'
+
+describe Tabry::Runner do
+  let(:config) { instance_double(Tabry::Models::Config) }
+  let(:state) { instance_double(Tabry::State) }
+
+  before do
+    allow(Tabry::ConfigLoader).to receive(:load).with(name: 'configname').and_return(config)
+  end
+
+  subject do
+    described_class.new(config_name: 'configname')
+  end
+
+  describe '#parse' do
+    it 'calls the machine and makes a result' do
+      expect(Tabry::Machine).to receive(:run).with(config, %w[foo bar]).and_return state
+      res = subject.parse(%w[foo bar])
+      expect(res).to be_a(Tabry::Result)
+      expect(res.state).to eq(state)
+      expect(res.config).to eq(config)
+    end
+  end
+
+  describe '#options' do
+    it 'runs OptionsFinder' do
+      res = instance_double(Tabry::Result)
+      expect(subject).to receive(:parse).with(%w[foo bar]).and_return res
+      expect(Tabry::OptionsFinder).to receive(:options).with(res, 'waz').and_return %w[a b c]
+      expect(subject.options(%w[foo bar], 'waz')).to eq(%w[a b c])
+    end
+  end
+end
