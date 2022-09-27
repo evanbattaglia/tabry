f810ebf16d74c192673d41922f3907d25cb7da65
Evan Battaglia <ebattaglia@instructure.com>
2022-09-27 09:40:45 -0600

    hack to speed-up non-completion paths
    you can put requires and other slow stuff after a config block in
    AllInOne.

--- lib/tabry/cli/all_in_one.rb
+++ lib/tabry/cli/all_in_one.rb
@@ -11,23 +11,38 @@ module Tabry
           require_relative '../config_builder'
           conf = Tabry::ConfigBuilder.build(**opts, &blk)
           instance_variable_set(:@tabry_all_in_one_config, conf)
+
+          # Hack to avoid processing block any more if only running completion
+          # That way you an have expensive requires after the include
+          if ARGV.first == "completion" and ARGV.length == 3 && ARGV[2].to_s =~ /^[0-9]+$/
+            throw :run_completion, true
+          end
         end
       end
 
       def self.run(cli: nil, config: nil, &blk)
         cli ||= Class.new(AllInOneBase)
-        cli.module_eval(&blk) if blk
+        run_completion = catch(:run_completion) do
+          cli.module_eval(&blk) if blk
+          nil
+        end
+
         config ||= cli.instance_variable_get(:@tabry_all_in_one_config)
 
-        if config.main.subs.by_name['completion']
-          require_relative '../shells/bash'
+        if run_completion
           require_relative '../shells/bash/wrapper'
+          Tabry::Bash::Wrapper.run(ARGV[1], ARGV[2], config: config)
+        end
+
+        if config.main.subs.by_name['completion']
           completion_mixin = Module.new do
             def completion__bash
+              require_relative '../shells/bash'
               puts Tabry::Shells::Bash.generate_self
             end
 
             def completion
+              require_relative '../shells/bash/wrapper'
               config = self.class.instance_variable_get(:@tabry_all_in_one_config)
               Tabry::Bash::Wrapper.run(args.cmd_line, args.comp_point, config: config)
             end
